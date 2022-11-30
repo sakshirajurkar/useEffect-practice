dc12d208b4fdd8fa0124c0e857f964925b4f2b8d
sakshi <sakshirajurkar09@gmail.com>
2022-11-30 09:55:35 -0800

    useEffect practice added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect,useState} from "react";
 function App() {
+  const [count,setCount] =useState(0);
+  const [age,setaAge] = useState(0);
+ useEffect (()=>{
+  console.log('App component loaded');
+ },[]);
+ useEffect (()=>{
+   console.log ('Count ={count}, Age ={age}');
+ },[count,age]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+      <h1>localStorage</h1>
+      <h2>Count:{count}</h2>
+      <h2>Age:{age}</h2>
+      <button onClick = {() => {setCount(count+1)}}>Increase Count</button>
+      <button onClick = {() => {setCount (count+1)}}>Increase Age</button>
+     </>
   );
 }
 
